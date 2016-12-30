0866c5a38c7b8a7d406ea9f5619f474f0eb7c89d
spasticarm0x15dec <spasticarm0x15dec@gmail.com>
2016-12-30 21:09:33 +0300

    procedure pracytice

--- firstprog/diamondp_char.pas
+++ firstprog/diamondp_char.pas
@@ -1,5 +1,8 @@
 program omm_char_diamond;
 
+
+
+
 procedure PrintSpaces(count: integer);
 var 
 	i: integer;
@@ -8,19 +11,51 @@ begin
 		write(' ');
 end;
 
-procedure PrintLinesOfDiamond(n, k: integer; s: string);
-var len: integer;
+
+
+
+{Print the upper part of diamond with odd numbers of symbols of entered message}
+procedure PrintUpperOdd(n, k: integer; s: string);
+
+var len, l: integer;
 begin
+
 	len := length(s);
+	l := ((k - 1)*len*2-len) div 2;
+
 	PrintSpaces((n + 1 - k)*len);
 	write(s);
-	if k > 1 then
-		begin
-			PrintSpaces((2*k - 3)*len);
-			write(s);
-		end;	
-	writeln
+
+if k = 2 then
+	begin
+		PrintSpaces(l);
+		write('@');
+		PrintSpaces(l);
+	end
+else
+
+if k = n + 1 then
+        begin
+	         PrintSpaces(l - 1);
+		 write('0#0');
+		 PrintSpaces(l - 1);
+	end
+else
+
+if k > 1 then
+	begin
+		PrintSpaces(((k - 1)*len*2-len) div 2);
+		write('O');
+		PrintSpaces(((k - 1)*len*2-len) div 2)
 end;
+
+if k > 1 then
+	write(s);
+writeln;
+end;
+
+
+
 
 var h, k, n: integer; s: string;
 begin
@@ -32,9 +67,9 @@ begin
 	until (h > 1) and (h mod 2 = 1);
 	n := h div 2;
 	for k := 1 to n + 1 do
-		PrintLinesOfDiamond(n, k, s);
+		PrintUpperOdd(n, k, s);
 	for K := n downto 1 do
-		PrintLinesOfDiamond(n, k, s)
+		PrintUpperOdd(n, k, s)
 end.		
 		
 
